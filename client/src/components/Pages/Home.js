20fa839cfaac526073cd3252848cb3a61f3ae1db
David Weid <david.weid.2@gmail.com>
2019-09-26 14:10:13 -0400

    Created utils dir with API (link front end to backend). Added test functions into <Home/>. Installed axios.

--- client/src/components/Pages/Home.js
+++ client/src/components/Pages/Home.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import API from "../../utils/API";
 import "./Pages.css";
 
 const Home = () => {
+  const [testData, setData] = useState(null);
+
+  const callAPI = () => {
+    API.testAPI()
+      .then(res => setData(res)).then(showData())
+      .catch(err => console.error(err));
+  };
+
+  const showData = () => console.log(testData);
 
   return (
     <main>
-      <h1 className="text-color">Home Page</h1>
+      <h1>Home Page</h1>
+      <button onClick={() => callAPI()}>Test API</button>
     </main>
   );
 };
---
+++ client/src/utils/API.js
@@ -0,0 +1,7 @@
+import Axios from "axios";
+
+export default {
+  testAPI() {
+    return Axios.get("/api/test");
+  }
+};
