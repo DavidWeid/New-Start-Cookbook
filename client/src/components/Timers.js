09d193ed3ee3bae4e6ce89b7b40c1076215ad805
David Weid <david.weid.2@gmail.com>
2020-12-12 12:06:15 -0500

    timer logic. remove keypad component

--- client/src/components/Timers.js
+++ client/src/components/Timers.js
@@ -1,21 +1,19 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useRef } from "react";
 import "./assets/css/timer.css";
 
 const Timers = () => {
-  // const [timers, setTimers] = useState([
-  //   {
-  //     startTime: 0,
-  //     isPaused: true,
-  //     remainingTime: 0
-  //   }
-  // ]);
-
   const [desiredTime, setDesiredTime] = useState([
     {
       number: undefined,
       timeType: ""
     }
   ]);
+
+  const [timerHours, setTimerHours] = useState("00");
+  const [timerMinutes, setTimerMinutes] = useState("00");
+  const [timerSeconds, setTimerSeconds] = useState("00");
+
+  let interval = useRef();
 
   const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -25,11 +23,9 @@ const Timers = () => {
     const updatedDesiredTime = [...desiredTime];
     const input = parseInt(e.target.dataset.number);
 
-    if (updatedDesiredTime[0].number === undefined && input === 0) {
-      return;
-    } else if (updatedDesiredTime.length === 6) {
-      return;
-    } else if (updatedDesiredTime[0].number === undefined && input !== 0) {
+    if (updatedDesiredTime[0].number === undefined && input === 0) return;
+    else if (updatedDesiredTime.length === 6) return;
+    else if (updatedDesiredTime[0].number === undefined && input !== 0) {
       updatedDesiredTime[0] = { number: input, timeType: "s" };
       setDesiredTime(updatedDesiredTime);
     } else if (updatedDesiredTime.length < 2) {
@@ -60,13 +56,36 @@ const Timers = () => {
   const undoLastTimerKey = () => {
     const updatedDesiredTime = [...desiredTime];
 
-    if (updatedDesiredTime[0].number === undefined) {
-      return;
-    } else if (updatedDesiredTime.length === 1) {
+    if (updatedDesiredTime[0].number === undefined) return;
+    else if (updatedDesiredTime.length === 1) {
       setDesiredTime([{ number: undefined, timeType: "" }]);
     } else {
       updatedDesiredTime.pop();
-      setDesiredTime(updatedDesiredTime);
+
+      if (updatedDesiredTime.length === 1 || updatedDesiredTime.length === 2) {
+        for (let i = 0; i < updatedDesiredTime.length; i++) {
+          updatedDesiredTime[i].timeType = "s";
+        }
+        setDesiredTime(updatedDesiredTime);
+      } else if (updatedDesiredTime.length === 3) {
+        updatedDesiredTime[0].timeType = "m";
+        updatedDesiredTime[1].timeType = "s";
+        updatedDesiredTime[2].timeType = "s";
+        setDesiredTime(updatedDesiredTime);
+      } else if (updatedDesiredTime.length === 4) {
+        updatedDesiredTime[0].timeType = "m";
+        updatedDesiredTime[1].timeType = "m";
+        updatedDesiredTime[2].timeType = "s";
+        updatedDesiredTime[3].timeType = "s";
+        setDesiredTime(updatedDesiredTime);
+      } else if (updatedDesiredTime.length === 5) {
+        updatedDesiredTime[0].timeType = "h";
+        updatedDesiredTime[1].timeType = "m";
+        updatedDesiredTime[2].timeType = "m";
+        updatedDesiredTime[3].timeType = "s";
+        updatedDesiredTime[4].timeType = "s";
+        setDesiredTime(updatedDesiredTime);
+      }
     }
   };
 
@@ -77,8 +96,31 @@ const Timers = () => {
   };
 
   const startTimer = () => {
-    let timeEntered = desiredTime;
+    const timeEntered = desiredTime;
+    const target = 0;
+
     console.log(timeEntered); // [{ number: #, timeType: "h/m/s"}] up to length 6
+
+    let hoursEntered;
+    let minutesEntered;
+    let secondsEntered = 0;
+
+    // if (timeEntered[0].number === undefined) return;
+
+    if (timeEntered.length < 3) {
+      for (let i = 0; i < timeEntered.length; i++) {
+        secondsEntered += timeEntered[i].number;
+      }
+
+      console.log(secondsEntered);
+    }
+
+    interval = setInterval(() => {
+      const hours = 0;
+      const minutes = 0;
+      const seconds = secondsEntered;
+      setTimerSeconds(seconds);
+    }, 1000);
   };
 
   const pauseTimer = () => {};
@@ -131,6 +173,7 @@ const Timers = () => {
         </div>
       </div>
       {displayedTime}
+      {timerHours}:{timerMinutes}:{timerSeconds}
     </Fragment>
   );
 };
