3d00bc522aa49ec46f2aeba7f7344d80917a0c31
David Weid <david.weid.2@gmail.com>
2021-01-08 19:42:39 -0500

    Move the 0 key in the timer

--- client/src/components/Timers.js
+++ client/src/components/Timers.js
@@ -107,7 +107,7 @@ const Timers = () => {
       .classList.toggle("display-none");
   };
 
-  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
   const handleInputChange = (e) => {
     console.log(e.target);
